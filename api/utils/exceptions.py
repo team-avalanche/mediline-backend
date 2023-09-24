cedbbd35b6f402bec0524a065176bbd96400c3cf
aahnik <daw@aahnik.dev>
2023-09-24 22:54:12 +0530

    add new exceptions, and refactor some old ones

--- api/utils/exceptions.py
+++ api/utils/exceptions.py
@@ -11,6 +11,14 @@ patient_profile_not_found_exc = HTTPException(
     status_code=status.HTTP_404_NOT_FOUND, detail="Patient Profile Not Found"
 )
 
+
+def invalid_doc_avlb_details_exc(msg):
+    return HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail=f"The data for Doctor Availability is invalid. \n {msg}",
+    )
+
+
 doc_avlb_details_not_found_exc = HTTPException(
     status_code=status.HTTP_404_NOT_FOUND,
     detail="Doctor Availability Details Not Found",
@@ -26,8 +34,11 @@ method_not_allowed_for_patient = HTTPException(
     detail="The method you are trying to access is only for doctor users.",
 )
 
+perm_denied_exc = HTTPException(
+    status_code=status.HTTP_403_FORBIDDEN, detail="Permission Denied."
+)
 
-invalid_cancellation_request = HTTPException(
+invalid_cncl_req_exc = HTTPException(
     status_code=status.HTTP_400_BAD_REQUEST,
     detail="Cant cancel appointment",
 )
