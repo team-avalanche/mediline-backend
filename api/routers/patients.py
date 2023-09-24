b7cd8f4b88d014758bd923ed929a9bc0a0b28c32
aahnik <daw@aahnik.dev>
2023-09-24 13:02:11 +0530

    udpate patient endpoints
    - a patient can check my-profile
    - a doctor can get profile of a patient who has
    atleast one scheduled or completed appointment

--- api/routers/patients.py
+++ api/routers/patients.py
@@ -1,19 +1,22 @@
+from beanie import PydanticObjectId
 from fastapi import APIRouter
 
+from api.models.appointments import Appointment
 from api.models.patients import PatientProfile, PatientProfileIn
-from api.utils.current_user import Patient
+from api.utils.current_user import Doctor, Patient
+from api.utils.enum_types import AppointmentStatus
 from api.utils.exceptions import patient_profile_not_found_exc
 
 router = APIRouter(prefix="/patient", tags=["Patients"])
 
 
-@router.get("/profile")
-async def get_patient_profile(user: Patient) -> PatientProfile:
+@router.get("/my-profile")
+async def get_patient_self_profile(user: Patient) -> PatientProfile:
     return await PatientProfile.find_one(PatientProfile.id == user.id)
 
 
-@router.patch("/profile")
-async def update_patient_profile(profile_update: PatientProfileIn, user: Patient):
+@router.patch("/my-profile")
+async def update_patient_self_profile(profile_update: PatientProfileIn, user: Patient):
     patient = await PatientProfile.find_one(PatientProfile.id == user.id)
     if not patient:
         raise patient_profile_not_found_exc
@@ -23,3 +26,19 @@ async def update_patient_profile(profile_update: PatientProfileIn, user: Patient
     await patient.save()
 
     return patient.id
+
+
+async def get_patient_profile(
+    patient_id: PydanticObjectId, user: Doctor
+) -> PatientProfile:
+    # privacy
+    # a doctor who has atleast appointment with patient will be able to view his profile
+    aptmnts = Appointment.find_many(
+        Appointment.doctor_id == user.id,
+        Appointment.status != AppointmentStatus.cancelled,
+    )
+    if len(aptmnts) > 0:
+        patient_profile = await PatientProfile.find_one(PatientProfile.id == patient_id)
+        if not patient_profile:
+            raise patient_profile_not_found_exc
+        return patient_profile
