ada447350a141a2e5c81f061a26c5244af3035b5
aahnik <daw@aahnik.dev>
2023-09-24 12:37:15 +0530

    update /doctor api endpoints
    - distinguish between self check and check by id
    - my-profile and my-availability
    endpoints works for currently logged in doctor
    - get-doctor-availability and get-doctor-profile endpoints work
    for any active user

--- api/routers/doctors.py
+++ api/routers/doctors.py
@@ -2,7 +2,7 @@ from beanie import PydanticObjectId
 from fastapi import APIRouter
 
 from api.models.doctors import DoctorAvailability, DoctorProfile, DoctorProfileIn
-from api.utils.current_user import Doctor, GcauDep
+from api.utils.current_user import ActiveUser, Doctor
 from api.utils.exceptions import (
     doc_avlb_details_not_found_exc,
     doc_profile_not_found_exc,
@@ -12,20 +12,36 @@ router = APIRouter(prefix="/doctor", tags=["Doctors"])
 
 
 @router.get("/get-all-doctors")
-async def get_all_doctors(user: GcauDep):
+async def get_all_doctors(user: ActiveUser):
     return await DoctorProfile.find_all().to_list()
 
 
-@router.get("/profile")
-async def get_doctor_profile(user: Doctor) -> DoctorProfile:
+@router.get("/get-doctor-profile")
+async def get_doctor_profile(doctor_id: PydanticObjectId, user: ActiveUser):
+    doctor_profile = DoctorProfile.find_one(DoctorProfile.id == doctor_id)
+    if not doctor_profile:
+        raise doc_profile_not_found_exc
+    return doctor_profile
+
+
+@router.get("/get-doctor-availability")
+async def get_doctor_availability(doctor_id: PydanticObjectId, user: ActiveUser):
+    doc_avlb = DoctorAvailability.find_one(DoctorProfile.id == doctor_id)
+    if not doc_avlb:
+        raise doc_avlb_details_not_found_exc
+    return doc_avlb
+
+
+@router.get("/my-profile")
+async def get_doctor_self_profile(user: Doctor) -> DoctorProfile:
     doctor = DoctorProfile.find_one(DoctorProfile.id == user.id)
     if not doctor:
         raise doc_profile_not_found_exc
     return doctor
 
 
-@router.patch("/profile")
-async def update_doctor_details(
+@router.patch("/my-profile")
+async def update_doctor_self_profile(
     profile_details: DoctorProfileIn, user: Doctor
 ) -> PydanticObjectId:
     doctor = DoctorProfile.find_one(DoctorProfile.id == user.id)
@@ -36,16 +52,16 @@ async def update_doctor_details(
     return doctor.id
 
 
-@router.get("/availability")
-async def get_doctor_availability(user: Doctor) -> DoctorAvailability:
+@router.get("/my-availability")
+async def get_doctor_self_availability(user: Doctor) -> DoctorAvailability:
     doc_avlb = DoctorAvailability.find_one(DoctorAvailability.id == user.id)
     if not doc_avlb:
         raise doc_avlb_details_not_found_exc
     return doc_avlb
 
 
-@router.patch("/availability")
-async def update_doctor_availability(
+@router.patch("/my-availability")
+async def update_doctor_self_availability(
     doc_avlb_update: DoctorAvailability, user: Doctor
 ) -> PydanticObjectId:
     doc_avlb = DoctorAvailability.find_one(DoctorProfile.id == user.id)
